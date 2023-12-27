f438489ae112906020c83b209cf0839740c13874
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-26 22:18:11 -0500

    _

--- lib/nmapa/nmapa.h
+++ lib/nmapa/nmapa.h
@@ -5,11 +5,39 @@
 #endif
 
 void nmap_scan_aggressive(const char* url) {
-    #if LEVEL || 1
+    #if LEVEL == 1
         printf("Scanning %s\n", url);
         char command[100];
         sprintf(command, "nmap -A %s", url);
         system(command);
     
+    #endif
+}
+
+void nmap_verbose_scan(const char* url){
+    #if LEVEL == 2
+        printf("Scanning %s\n", url);
+        char command[100];
+        sprintf(command, "nmap -vv %s", url);
+        system(command);
+    
+    #endif
+}
+
+void nmap_decoy_scan(const char* url){
+    #if LEVEL == 2 
+        printf("Scanning %s\n", url);
+        char command[100];
+        sprintf(command, "nmap -D RND:3 %s", url);
+    
+    #endif
+}
+
+void nmap_version_system_scan(const char* url){
+    #if LEVEL ==  3
+        printf("Scanning %s\n", url);
+        char command[100];
+        sprintf(command, "nmap -sV %s", url);
+        system(command);
     #endif
 }
