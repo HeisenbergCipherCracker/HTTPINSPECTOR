9b044fecad47521a5eb4ef96d13d9c7dc37a12d0
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2023-12-26 21:18:02 -0500

    Basic changes

---
+++ lib/HTTP/HTTP.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+#define LEVEL NULL
+
+
+void send_http_req(const char* url){
+    CURL *curl;
+    CURLcode res;
+    curl = curl_easy_init();
+    if (curl){
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            fprintf(LEVEL, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+        curl_easy_cleanup(curl);
+    }
+
+}
